3636a49c32be030b437ccdf92aa6e259a81a805f
chengyalin <1510823536@qq.com>
2018-06-08 11:29:06 +0800

    6.8

---
+++ src/components/phoneLogin.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="headBar">
+      <div class="navBar"><img src="/static/img/goBack.png" alt="" class="goBack" @click="goBackPage"><p class="navName">{{title}}</p></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'footerBar',
+  props:{
+  	title:{
+  		type:String,
+  		defalut: ''
+  	}
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    goBackPage () {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.headBar{height: 50px;line-height: 50px;width: 100%;}
+.navBar{height: 50px;line-height: 50px;width: 100%;background:rgba(247,247,247,1);position: fixed;top:0;left: 0;z-index: 10;}
+.goBack{display: block; width: 8px;height: 14px;padding: 4px;position: absolute;top: 16px;left: 20px;}
+.navName{font-size: 18px;color:rgba(0,0,0,1);text-align: center;height: 50px;line-height: 50px;}
+
+</style>
